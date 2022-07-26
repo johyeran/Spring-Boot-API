8d3f3496acfff43ec65d2071faba568b74778508
johyeran <hr.jo@dnd-lab.com>
2022-07-26 18:26:13 +0900

    service 생성

--- src/main/java/com/test/restapi/board/BoardCRUDController.java
+++ src/main/java/com/test/restapi/board/BoardCRUDController.java
@@ -1,22 +1,17 @@
 package com.test.restapi.board;
 
-import lombok.Builder;
+import com.test.restapi.board.application.BoardCRUDService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @RestController
 @RequestMapping ("/api/v1")
 public class BoardCRUDController {
-    Map<Integer, Object> boardList = new HashMap<Integer, Object>();
+    private final BoardCRUDService boardCRUDService;
 
-//    @GetMapping("/write")
-//    public void SetBoardList(Integer boardIdx, Object boardContents) {
-//        this.boardList.put(boardIdx, boardContents);
-//    }
+    @Autowired
+    public BoardCRUDController(final BoardCRUDService boardCRUDService) {
+        this.boardCRUDService = boardCRUDService;
+    }
 }
---
+++ src/main/java/com/test/restapi/board/application/BoardCRUDService.java
@@ -0,0 +1,11 @@
+package com.test.restapi.board.application;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BoardCRUDService {
+    public BoardCRUDService () {
+
+    }
+}
